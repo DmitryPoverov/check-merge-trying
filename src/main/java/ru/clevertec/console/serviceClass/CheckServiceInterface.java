e6104e78f71dae2bb9ebbdaef191bd047a4d3877
DmitryPoverov <did.ink@mail.ru>
2022-06-23 15:30:23 +0300

    All methods were moved to a service class

--- src/main/java/ru/clevertec/console/serviceClass/CheckServiceInterface.java
+++ src/main/java/ru/clevertec/console/serviceClass/CheckServiceInterface.java
@@ -9,22 +9,13 @@ import java.util.List;
 public interface CheckServiceInterface {
 
     void parseParamsToGoodsAndCard(String[] arguments, Check check);
-
     void checkData(String[] strings, String invalidDataFilePath, Check check);
-
     List<String> createList(Check check);
-
     void printToFile(String path, Check check);
-
     List<String> printToStringList(Check check);
-
     boolean isValid(String productString);
-
     String[] convertStringToArray(String text, String regex);
-
     String convertPathStringToTextString(String path, String delimiter) throws IOException;
-
     List<ParamMapper> setParamMapper(List<String> params, String regex);
-
     void printToConsoleFromFile(Check check);
 }
