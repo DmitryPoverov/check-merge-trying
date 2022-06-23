79f1652c9d9abd8933fbc015f59a97e2a2e51cd5
DmitryPoverov <did.ink@mail.ru>
2022-06-23 15:21:00 +0300

    Service interface was created

---
+++ src/main/java/ru/clevertec/console/serviceClass/CheckServiceInterface.java
@@ -0,0 +1,30 @@
+package ru.clevertec.console.serviceClass;
+
+import ru.clevertec.console.Check;
+import ru.clevertec.console.ParamMapper;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface CheckServiceInterface {
+
+    void parseParamsToGoodsAndCard(String[] arguments, Check check);
+
+    void checkData(String[] strings, String invalidDataFilePath, Check check);
+
+    List<String> createList(Check check);
+
+    void printToFile(String path, Check check);
+
+    List<String> printToStringList(Check check);
+
+    boolean isValid(String productString);
+
+    String[] convertStringToArray(String text, String regex);
+
+    String convertPathStringToTextString(String path, String delimiter) throws IOException;
+
+    List<ParamMapper> setParamMapper(List<String> params, String regex);
+
+    void printToConsoleFromFile(Check check);
+}
